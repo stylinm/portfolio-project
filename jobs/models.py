8f97f6f0fba91dee527fc8ba298614127d30ffa9
mike <mikeblicker@gmail.com>
2020-04-02 16:07:05 -0400

    jobs templates and blog templates views changed

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class Job(models.model):
+class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
